dae5955d022c1bcc9e0e2e39a48a009556180ef3
IM CHANJIN <110803149+AACHANJINAA@users.noreply.github.com>
2026-08-14 14:07:38 +0900

    기본 프로젝트 커밋 + 사용할 robot mesh 추가 및 animation retargeting 완료

---
+++ Source/Aeterna/Aeterna.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Aeterna.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aeterna, "Aeterna" );
+
+DEFINE_LOG_CATEGORY(LogAeterna)
---
+++ Source/Aeterna/AeternaGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AeternaGameMode.h"
+
+AAeternaGameMode::AAeternaGameMode()
+{
+	// stub
+}
